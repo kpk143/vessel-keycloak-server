e4e080484134e93c98f5b8ee9c34a55b62f7e02f
kpk143 <kpk338@hotmail.com>
2026-03-12 16:07:17 +0530

    Update KC_HOSTNAME in start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -6,5 +6,5 @@ docker run -p 8080:8080 \
   -e KEYCLOAK_ADMIN=admin \
   -e KEYCLOAK_ADMIN_PASSWORD=admin \
   -e KC_PROXY_HEADERS=xforwarded \
-  -e KC_HOSTNAME=glowing-capybara-qv5794jj66f4g4p-8080.app.github.dev \
+  -e KC_HOSTNAME=ideal-memory-q7x5w56r7jg7fxw5w-8080.app.github.dev \
   quay.io/keycloak/keycloak:latest start-dev
